05d1714fa78ad6c42f7336b80f88348805da84eb
Sergio Donato Barbosa <sergiodonatobarbosa@gmail.com>
2026-02-23 10:55:54 -0300

    Estrutura de atendimentos pronta e início da lógica de análise de sentimentos

--- analisador.py
+++ analisador.py
@@ -3,13 +3,17 @@ import sqlite3
 conexao = sqlite3.connect("meubanco.db")
 cursor = conexao.cursor()
 
+with open("atendimentos.txt", "r", encoding="utf-8") as arquivo:
+    for linha in arquivo:
+        dados = linha.strip().split(",")
+        
+        cursor.execute("""INSERT INTO atendimentos (operador, data_hora, tma, transcricao, sentimento)
+               VALUES (?, ?, ?, ?, ?)
+
+               """, (dados[0], dados[1], int(dados[2]), dados[3], "pendente"))
 
 
-
-
-#cursor.execute("DROP TABLE operadores")
-
-cursor.execute("SELECT * FROM operadores")
+cursor.execute("SELECT * FROM atendimentos")
 
 print(cursor.fetchall())
 
