a233de6d848c80eb95715280f3b0c994aba62f59
Sergio Donato Barbosa <sergiodonatobarbosa@gmail.com>
2026-03-03 11:12:35 -0300

    Finalizando lógica de análise de qualidade e tratamento de duplicatas

--- analisador.py
+++ analisador.py
@@ -1,49 +1,78 @@
 import sqlite3
 
-conexao = sqlite3.connect("meubanco.db")
-cursor = conexao.cursor()
+def classificar_sentimento(transcricao):
+    palavras_criticas = ["procon", "cancelar", "absurdo", "indevida", "bravo", "ruim", "péssimo"]
+    texto_analise = transcricao.lower()
 
-palavras_criticas = ["procon", "cancelar", "absurdo", "indevida", "bravo", "ruim", "péssimo"]
+    for palavra in palavras_criticas:
+        if palavra in texto_analise:
+            return "crítico"
+    
+    return "bom"
+
+try:
+    conexao = sqlite3.connect("database_01.db")
+    cursor = conexao.cursor()
+    
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS atendimentos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            operador TEXT,
+            data_hora TEXT,
+            protocolo INTEGER UNIQUE,
+            transcricao TEXT,
+            sentimento TEXT
+        )
+    """)
+
+    print("Iniciando análise de atendimentos...")
+    print("-" * 50)
+
+    conexao = sqlite3.connect("database_01.db")
+    cursor = conexao.cursor()
+    palavras_criticas = ["procon", "cancelar", "absurdo", "indevida", "bravo", "ruim", "péssimo"]
+
+    with open("atendimentos.txt", "r", encoding="utf-8") as arquivo:
+        for linha in arquivo:
+
+            try:
+                
+                dados = linha.strip().split(",")
+
+                operador, data, protocolo, transcricao = dados[0], dados[1], int(dados[2]), dados[3]
+
+                sentimento = classificar_sentimento(transcricao)
+
+                cursor.execute("""
+                               INSERT OR IGNORE INTO atendimentos (operador, data_hora, protocolo, transcricao, sentimento)
+                                VALUES (?, ?, ?, ?, ?)""", (operador, data, protocolo, transcricao, sentimento))
+
+            except (ValueError, IndexError) as e:
+                print(f"Erro ao processar linha: {linha.strip()} - {e}")
+                continue
+
+    cursor.execute("SELECT operador, protocolo FROM atendimentos WHERE sentimento = 'crítico'")
+    criticos = cursor.fetchall()
+
+    print(f"\nFoi encontrado um total de {len(criticos)} atendimentos críticos:\n")
+
+    for c in criticos:
+        print(f"Operador: {c[0]} | Protocolo de Atendimento: {c[1]}")
+
+    print("-" * 50)
+
+    cursor.execute("SELECT * FROM atendimentos")
 
 
+    dados = cursor.fetchall()
+    print(f"\nDados armazenados no banco de dados:\n")
+    for dado in dados:
+        print(dado)
 
-print("Iniciando análise de atendimentos...")
+except sqlite3.Error as e:
+    print(f"Erro ao acessar o banco de dados: {e}")
 
-print("-" * 50)
-
-
-with open("atendimentos.txt", "r", encoding="utf-8") as arquivo:
-    for linha in arquivo:
-        dados = linha.strip().split(",")
-
-        operador, data, protocolo, transcricao = dados[0], dados[1], int(dados[2]), dados[3]
-
-        sentimento = "bom"
-        texto_analise = transcricao.lower()
-
-        for palavra in palavras_criticas:
-            if palavra in texto_analise:
-                sentimento = "crítico"
-                break
-        
-        '''cursor.execute("""INSERT INTO atendimentos (operador, data_hora, protocolo, transcricao, sentimento)
-               VALUES (?, ?, ?, ?, ?)
-
-               """, (operador, data, protocolo, transcricao, sentimento))'''
-
-
-cursor.execute("SELECT operador, protocolo FROM atendimentos WHERE sentimento = 'crítico'")
-criticos = cursor.fetchall()
-
-print(f"\nFoi encontrado um total de {len(criticos)} atendimentos críticos:\n")
-
-for c in criticos:
-    print(f"Operador: {c[0]} | Protocolo de Atendimento: {c[1]}")
-
-#conexao.commit()
-
-conexao.close()
-
-print("-" * 50)
-
-print("\nAnálise de atendimentos concluída.")
+finally:    
+    if conexao:
+        conexao.commit()
+        conexao.close()
